b26e2f38750e112acfdc72aa15ff34a1aaced7f1
DESKTOP-MBRQ737\Eva <evastylova@gmail.com>
2023-02-12 20:15:50 +0100

    Ukol 4 JS1 - retezce

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 // tady je místo pro náš program
 
 
+let email = prompt('Zadej svůj email.')
+let atIndex
+atIndex = email.indexOf('@');
+let jmeno
+jmeno = email.slice(0, atIndex);
 
+
+let domena
+domena = email.slice(atIndex +1, email.length+1);
+
+const parsedEmail = {
+	userName: jmeno,
+	domain: domena
+}
+document.body.innerHTML += "<p>Uživatelské jméno: "+ parsedEmail.userName +"</p>";
+document.body.innerHTML += "<p>Doména: "+ parsedEmail.domain +"</p>";
 
